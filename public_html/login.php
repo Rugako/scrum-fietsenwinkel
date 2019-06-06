c1ad08aab848c74ac00513db82da2f1c75d36b0b
LAPTOP-0PE5PL7P\Gebruiker <denzel.delsink@student.graafschapcollege.nl>
2019-06-06 09:23:35 +0200

    some workin

--- public_html/login.php
+++ public_html/login.php
@@ -1,22 +1,71 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <h2>Login</h2>
+    <form action="" method="post">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <label>Email</label>
+            <input type="text" name="email" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" value="">
+
+
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" name="Submit">
+        </div>
+        <p>Dont have a account? <a href="register.php">Sign up here</a>.</p>
+    </form>
+</div>
+</body>
+</html>
 <?php
-include('includes.php');
-$database = new Database();
-?>
-<table border="2";>
-    <tr>
-        <td><b>Id  </b></td>
-        <td><b>Voornaam  </b></td>
-        <td><b>Achternaam  </b></td>
-        <td><b>Email  </b></td>
-    </tr>
-    <?php
-    $query= "SELECT * FROM user";
-    $result= mysqli_query($connection,$query);
-    while ($row = mysqli_fetch_assoc($result)){
-        ?> <tr>
-        <td><?php echo $row["ID"]?></td>
-        <td><?php echo $row["vnaam"]?></td>
-        <td><?php echo  $row["anaam"]?></td>
-        <td><?php echo $row["email"]?></td>
-        </tr><?php }?>
-</table>
+
+
+
+
+$dbname ="fiets";
+$dbuser ="root";
+$dbpass="";
+$dbhost="localhost";
+
+$link = mysqli_connect($dbhost  ,$dbuser,$dbpass,$dbname)or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link,$dbname)or die ("Could not open the db'$dbname'");
+
+
+if(isset($_POST['Submit']))
+{
+
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $sql="SELECT * FROM klant WHERE Klant_email = '$email' AND Klant_Wachtwoord= '$password' ";
+    $result= $link->query($sql);
+  if($result->num_rows>0)
+  {
+      header("Location: index.php");
+  }
+
+
+
+
+
+
+
+
+}
+
---
+++ public_html/register.php
@@ -0,0 +1,133 @@
+<?php
+include ("Database.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <h2>Sign Up</h2>
+    <p>Please fill this form to create an account.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <label>Email</label>
+            <input type="text" name="email" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" value="">
+
+        </div>
+
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Voornaam</label>
+            <input type="text" name="voornaam" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Tussenvoegsel</label>
+            <input type="text" name="tussenvoegsel" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Achternaam</label>
+            <input type="text" name="achternaam" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Telefoon nummer</label>
+            <input type="text" name="tel" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Postcode</label>
+            <input type="text" name="postcode" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>WoonPlaats</label>
+            <input type="text" name="plaats" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Adres</label>
+            <input type="text" name="adres" class="form-control" value="">
+
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Adres 2</label>
+            <input type="text" name="adres2" class="form-control" value="">
+
+        </div>
+
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Geboortedatum</label>
+            <input type="text" name="gbdatum" class="form-control" value="">
+
+        </div>
+
+
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Submit" name="Submit">
+        </div>
+        <p>Already have an account? <a href="register.php">Login here</a>.</p>
+    </form>
+</div>
+</body>
+</html>
+<?php
+
+
+
+
+$dbname ="fiets";
+$dbuser ="root";
+$dbpass="";
+$dbhost="localhost";
+
+$link = mysqli_connect($dbhost  ,$dbuser,$dbpass,$dbname)or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link,$dbname)or die ("Could not open the db'$dbname'");
+
+if(isset( $_POST['Submit']))
+{
+
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $voornaam=$_POST['voornaam'];
+    $tussenvoegsel=$_POST['tussenvoegsel'];
+    $achternaam=$_POST['achternaam'];
+    $postcode=$_POST['postcode'];
+    $woonplaats=$_POST['plaats'];
+    $adres=$_POST['adres'];
+    $adres2=$_POST['adres2'];
+    $tel=$_POST['tel'];
+
+    $sql = "INSERT INTO klant (Klant_Naam	
+,Klant_Tussenvoegsel,	Klant_Achternaam	,Klant_Postcode	
+,Klant_Plaats,	Klant_Adres	,Klant_Adres2	,Klant_TelefoonNum
+	,Klant_Email,	Klant_Wachtwoord)	 VALUES
+            ('$voornaam', '$tussenvoegsel', '$achternaam'
+            ,'$postcode','$woonplaats',
+            '$adres','$adres2','$tel','$email','$password')";
+      if($link->query($sql)===true)
+      {
+          header("Location:login.php");
+
+      }
+
+
+
+
+}
+
